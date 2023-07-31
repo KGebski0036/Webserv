fa2eee427fbca7f70ed510614fa9dcee46299ae4
Claudius J <digital.k.jack@gmail.com>
2023-07-31 13:39:57 +0200

    ServerInstanceConfig struct WIP

---
+++ inc/ServerInstanceConfig.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerInstanceConfig.hpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 12:49:32 by cjackows          #+#    #+#             */
+/*   Updated: 2023/07/31 13:04:15 by cjackows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <vector>
+
+struct ServerInstanceConfig
+{
+	std::vector<std::string>	_serverNames;
+	_host;
+	_port;
+};
