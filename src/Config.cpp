4ad725a029899b23fc42648e80edf16f0e4da382
Karol Gebski <kgebski@c2s18d112.42wolfsburg.de>
2023-07-31 14:55:33 +0200

    add separator by space in congiguration reading

--- src/Config.cpp
+++ src/Config.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Config.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: kgebski <kgebski@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 14:31:33 by cjackows          #+#    #+#             */
-/*   Updated: 2023/07/29 15:59:25 by cjackows         ###   ########.fr       */
+/*   Updated: 2023/07/31 14:54:13 by kgebski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,25 @@ void Config::readFile(std::string pathToFile)
 {
 	std::ifstream file;
 	std::string buffer;
+	std::string tmp;
+	std::stringstream ss;
+	
 	file.open(pathToFile.c_str());
 	if (!file.is_open())
 		throw MyException("Could not open a config file", __func__, __FILE__, __LINE__);
 
-//TODO ' ' and '\t' should be a separator :D
-	while (getline(file, buffer))
+	getline(file, buffer, '\0');
+	ss << buffer;
+
+	while (ss >> tmp)
 	{
-		_fileVector.push_back(buffer);
-		if (buffer.empty())
+		_fileVector.push_back(tmp);
+		if (tmp.empty())
 			_fileVector.pop_back();
 	}
 	if (_fileVector.empty())
 		throw MyException("File is empty...", __func__, __FILE__, __LINE__);
+		
 	for (size_t i = 0; i < _fileVector.size(); i++)
 	{
 		std::cout << SYS_MSG << BLUE << _fileVector[i] << E;
