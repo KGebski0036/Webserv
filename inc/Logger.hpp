da4cd8ce39656f9eb389568fccd80b5cf14161af
Claudius J <digital.k.jack@gmail.com>
2023-08-02 17:45:22 +0200

    logger finished

---
+++ inc/Logger.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:46:02 by cjackows          #+#    #+#             */
+/*   Updated: 2023/08/02 17:43:38 by cjackows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include "Colors.hpp"
+
+enum LogLevel {
+  NONE,
+  INFO,
+  DEBUG
+};
+
+class Logger {
+  public:
+	Logger(LogLevel level);
+	~Logger();
+
+	void print(LogLevel level, std::string str, bool error);
+	void print(LogLevel level, std::string color, std::string str, bool error);
+
+  private:
+	const LogLevel _level;
+	std::ofstream _logFile;
+
+	void printIO(std::string color, std::string str, bool error, char* time);
+	void printToFile(std::string str, bool error, char* time);
+
+	Logger();
+	Logger(const Logger&);
+	Logger& operator=(Logger const &);
+};
---
+++ src/Logger.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:53:29 by cjackows          #+#    #+#             */
+/*   Updated: 2023/08/02 17:42:58 by cjackows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Logger.hpp"
+
+void Logger::print(LogLevel level, std::string str, bool error) {
+	if (level <= _level) {
+		time_t rawtime;
+		struct tm* tm;
+		char buf[32];
+
+		time(&rawtime);
+		tm = localtime (&rawtime);
+		int ret = strftime(buf, 32, "%T", tm);
+		buf[ret] = '\0';
+
+		printToFile(str, error, buf);
+		printIO(YELLOW, str, error, buf);
+	}
+}
+
+void Logger::print(LogLevel level,std::string color, std::string str, bool error) {
+	if (level <= _level) {
+		time_t rawtime;
+		struct tm* tm;
+		char buf[32];
+
+		time(&rawtime);
+		tm = localtime (&rawtime);
+		int ret = strftime(buf, 32, "%T", tm);
+		buf[ret] = '\0';
+
+		printToFile(str, error, buf);
+		printIO(color, str, error, buf);
+	}
+}
+
+void Logger::printToFile(std::string str, bool error, char* time)
+{
+	if (error)
+		_logFile << "[" << time << "] " << "[ERROR] " << str << E;
+	else
+		_logFile << "[" << time << "] " << str << E;
+}
+
+void Logger::printIO(std::string color, std::string str, bool error, char* time)
+{
+	if (error)
+		std::cerr << G << "[" << time << "] " << C_ERROR << BOLD << str << E;
+	else
+		std::cout << G << "[" << time << "] " << color << str << E;
+}
+
+Logger::Logger(LogLevel level) : _level(level) {
+	_logFile.open("webserv.log", std::ios::out | std::ios::trunc);
+	if (!_logFile.is_open()) {
+		std::cerr << "Error opening log file" << E;
+	}
+}
+
+Logger::~Logger() {
+	if (_logFile.is_open())
+		_logFile.close();
+}
+
+Logger::Logger() : _level(NONE) {}
+Logger::Logger(const Logger& src) : _level(NONE) { (void)src; }
+Logger& Logger::operator=(Logger const& src) { (void)src; return *this; }
