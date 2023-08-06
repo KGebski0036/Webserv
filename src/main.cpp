0a836fc6bb5006aec8508c2896f7d007582de164
Claudius J <digital.k.jack@gmail.com>
2023-08-06 16:59:09 +0200

    Signals are ready

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 12:32:00 by cjackows          #+#    #+#             */
-/*   Updated: 2023/08/03 19:07:05 by cjackows         ###   ########.fr       */
+/*   Updated: 2023/08/06 16:58:40 by cjackows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 Logger logger(DEBUG);
 
+void signalHandler(int signal);
+
 int	main(int ac, char *av[])
 {
 	Config input(ac, av, &logger);
@@ -22,11 +24,24 @@ int	main(int ac, char *av[])
 		return 1;
 
 	Webserv server(input, &logger);
-
+	signal(SIGINT, signalHandler);
 	try
 	{
 		server.setup();
 		server.run();
 	} catch (const MyException &e) { std::cerr << e.what(); return 1;}
 	return 0;
+}
+
+void signalHandler(int signal) {
+	std::cout << E;
+	
+	if (signal == SIGINT)
+		logger.print(INFO, RED, "Received SIGINT signal. Exiting now...", 0);
+	else if (signal == SIGTERM)
+		logger.print(INFO, RED, "Received SIGTERM signal. Exiting now...", 0);
+	else
+		logger.print(INFO, RED, "Received unknown signal. Exiting now...", 0);
+
+	exit(signal);
 }
